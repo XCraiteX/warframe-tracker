624023c364ae08fda0c6ee028c42c532ede58f7a
CraiteX x REDLITE <rlcraitex@gmail.com>
2025-03-15 19:47:20 +0300

    small

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,4 +1,2 @@
-// export const API_URL = 'http://localhost:3202/warframe/api';
-export const API_URL = 'https://redlite.ru/warframe/api'
-// export const WARF_API_URL = 'http://localhost:3202/warframe/api';
-// export const WARF_API_URL = 'https://redlite.ru/warframe/api'
+export const API_URL = 'http://localhost:3202/warframe/api';
+// export const API_URL = 'https://redlite.ru/warframe/api';
